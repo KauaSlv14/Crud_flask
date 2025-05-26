3734660c066b0693076f7fcb90be2511bf6cee48
KauaSlv14 <zka18952@gmail.com>
2025-05-26 15:13:53 -0300

    Update database.py

--- database/database.py
+++ database/database.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 db= PostgresqlDatabase(
-    db_name=os.getenv('db_name'),
+    os.getenv('db_name'),
     user=os.getenv('db_user'),
     password=os.getenv('db_password'),
     host=os.getenv('db_host'),
